71c5e6c488afa332462e890aed45e2e75553e27c
Maryana Dey <Dey.maryana1989@gmail.com>
2023-03-02 13:46:18 +0100

    add react-router-dom

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,19 +1,10 @@
-// import logo from './logo.svg';
-import './App.css';
-
-import { Routes, Route, useLocation } from 'react-router-dom';
-
-
-
-
 
 
 function App() {
   return (
     <>
-      <Routes>
-        <Route />
-    </Routes>
+      <div>Hello world</div>
+      <button>button</button>
     </>
   );
 }
@@ -21,20 +12,3 @@ function App() {
 export default App;
 
 
-
-
-
-{/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
